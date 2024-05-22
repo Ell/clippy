be21dfac44274a45d81bbb28ae5bf7d9e6adba56
ell <ell@ell.dev>
2024-05-22 11:48:13 -0500

    updates

--- embedder.py
+++ embedder.py
@@ -1,38 +1,46 @@
 import json
 
-import chromadb
-from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
+# import chromadb
+# from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
 
 
-chroma_client = chromadb.HttpClient(host="100.73.70.105")
+# chroma_client = chromadb.HttpClient(host="100.73.70.105")
 
-embedding_function = OllamaEmbeddingFunction(
-    model_name="mxbai-embed-large",
-    url="http://localhost:11434/api/embeddings",
-)
+# embedding_function = OllamaEmbeddingFunction(
+#     model_name="mxbai-embed-large",
+#     url="http://localhost:11434/api/embeddings",
+# )
 
-collection = chroma_client.get_or_create_collection(
-    name="aita", embedding_function=embedding_function
-)
+# collection = chroma_client.get_or_create_collection(
+#     name="aita", embedding_function=embedding_function
+# )
 
 with open("aita.json", "r") as data:
     posts = json.loads(data.read())
 
+result = 0
 for post in posts:
-    contents = post["text"]
+    length = len(post["text"])
+    if length > result:
+        result = length
 
-    del post["text"]
+print(result)
 
-    print(f"Creating and inserting embedding for {post['title']}")
+# for post in posts:
+#     contents = post["text"]
 
-    try:
-        collection.add(
-            documents=[contents],
-            metadatas=[post],
-            ids=[post["id"]],
-        )
-    except Exception as e:
-        print(e)
+#     del post["text"]
+
+#     print(f"Creating and inserting embedding for {post['title']}")
+
+#     try:
+#         collection.add(
+#             documents=[contents],
+#             metadatas=[post],
+#             ids=[post["id"]],
+#         )
+#     except Exception as e:
+#         print(e)
 
 # search = collection.query(
 #     query_texts=["sex"],
